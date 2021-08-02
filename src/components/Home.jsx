a091a70d8002f3c81adb628f0a481947f272b7d6
unknown <bakaeveldos3@gmail.com>
2021-08-02 17:31:27 +0600

    create ProductList

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import ProductList from './ProductList';
 
 const Home = () => {
     return (
         <div>
-            qawsdgfhbkjnkjhgc
+            <ProductList />
         </div>
     );
 };
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductList = () => {
+    return (
+        <div>
+            szxdfgvhbjnkm,
+        </div>
+    );
+};
+
+export default ProductList;
