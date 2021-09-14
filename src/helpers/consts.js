6a93c73f2ff600e9d76ac4af63b0b44db167c372
SanzharKadyrkulov <kadyrkulov.980@gmail.com>
2021-09-14 16:15:46 +0600

    revert back

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -1,17 +1,20 @@
-export const JSON_API_PRODUCTS = 'https://mobile-legends-kg.herokuapp.com/api'
-export const AUTH_API_REG = 'https://intense-retreat-64750.herokuapp.com/auth/registration'
-export const AUTH_API_LOGI = "https://intense-retreat-64750.herokuapp.com/auth/login"
+// export const JSON_API_PRODUCTS = 'https://mobile-legends-kg.herokuapp.com/api'
+export const JSON_API_PRODUCTS = "http://localhost:8001/Products";
+export const AUTH_API_REG =
+  "https://intense-retreat-64750.herokuapp.com/auth/registration";
+export const AUTH_API_LOGI =
+  "https://intense-retreat-64750.herokuapp.com/auth/login";
 
 export const ACTIONS = {
-    GET_PRODUCTS: "GET_PRODUCTS",
-    GET_PRODUCT_DETAILS: "GET_PRODUCT_DETAILS",
-    GET_CART: "GET_CART",
-    GET_FAV: "GET_FAV",
-    AUTH_SUCCES: "AUTH_SUCCES",
-    AUTH_LOADING: "AUTH_LOADING",
-    AUTH_ERROR: "AUTH_ERROR",
-    CLEAR_AUTH_STATE: 'CLEAR_AUTH_STATE',
-    AUTH_LOGOUT: 'AUTH_LOGOUT'
-}
+  GET_PRODUCTS: "GET_PRODUCTS",
+  GET_PRODUCT_DETAILS: "GET_PRODUCT_DETAILS",
+  GET_CART: "GET_CART",
+  GET_FAV: "GET_FAV",
+  AUTH_SUCCES: "AUTH_SUCCES",
+  AUTH_LOADING: "AUTH_LOADING",
+  AUTH_ERROR: "AUTH_ERROR",
+  CLEAR_AUTH_STATE: "CLEAR_AUTH_STATE",
+  AUTH_LOGOUT: "AUTH_LOGOUT",
+};
 
 export const PRODUCTLIMIT = 3;
